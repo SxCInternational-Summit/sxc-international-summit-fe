05bf2ebab7e288a08e8c1a920642322a0d7f08bc
AtlanticNova <idiotcolony97@gmail.com>
2021-07-09 21:22:01 +0700

    debug footer
    masih belum responsive

--- component/footer.js
+++ component/footer.js
@@ -2,32 +2,29 @@ import {
     Image,
     Link,
     Flex,
-    Center,
     Text,
-    Stack
+    Stack,
+    HStack
 } from "@chakra-ui/react";
 import styles from "../styles/footer.module.scss"
 
 const Footer = () => {
     return(
-        <Flex h="192px" bgColor="#080818" w="100%" justifyContent="space-between" top={0}>
+        <HStack h="192px" bgColor="#080818" w="100%" justifyContent="space-between" top={0}>
             <Link href="/">
-                <Image src="/images/footerLogo.svg" alt="logo" ml="120px" cursor="pointer" />
-            </Link>
-
-            <Stack>
-                <Flex justifyContent="space-evenly" w="640px">
+                <Image src="/images/footerLogo.svg" alt="logo" ml="120px" cursor="pointer"/>
+            </Link>            
+            <Stack w="100%" alignItems="center" pr="300px" top={0}>
+                <Flex>
                     <Image src="/images/instagram.svg" alt="instagram" cursor="pointer"/>
-                    <Image src="/images/linkedIn.svg" alt="linkedIn" ml="30px" cursor="pointer"/>
-                    <Image src="/images/twitter.svg" alt="twitter" ml="30px" cursor="pointer"/>
+                    <Image src="/images/linkedIn.svg" alt="linkedIn" ml="16px" cursor="pointer"/>
+                    <Image src="/images/twitter.svg" alt="twitter" ml="16px" cursor="pointer"/>
                 </Flex>
-                <Center color="white" className={styles.footerFont} >
-                    <Text fontSize="md">
-                        (md) Copyright @2021 StudentsxCEOs 
-                    </Text>
-                </Center>
+                <Text fontSize="md" color="white" className={styles.footerFont}>
+                        Copyright @2021 StudentsxCEOs 
+                </Text>
             </Stack>
-        </Flex>
+        </HStack>
     )
 }
 
