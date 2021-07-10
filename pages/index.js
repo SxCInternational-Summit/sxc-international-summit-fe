6181b8e4f924faa5936a23491db55c7a581576c6
Daniel Andrew <danielandrewtt@gmail.com>
2021-07-10 14:13:39 +0700

    Changed icon logo

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,15 @@ import Footer from "../component/footer";
 import Navbar from "../component/navbar";
 import Temporary from "../component/temporary";
 
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>SxcIntersummit 2021</title>
+      <link rel="icon" href="/icon.svg" />
+    </Head>
     <Navbar />
     <Temporary/>
     <Footer />
