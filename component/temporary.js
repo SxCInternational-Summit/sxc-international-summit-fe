313381f41cbe7d4948bee1622e58d06b664d8c2f
abduh <muhammad.abdurahman@ui.ac.id>
2021-07-10 13:57:19 +0700

    fixed margin top

--- component/temporary.js
+++ component/temporary.js
@@ -12,9 +12,9 @@ import styles from "../styles/temporary.module.scss";
 const Temporary = () => {
     return(
         <Center className={styles.backgroundImage} mt="4rem">
-            <Stack>
+            <Stack pt="64px">
                 <Image src="/images/temporaryLogo.svg" alt="title logo" height="128px" />
-                <Text className={styles.titleFont} mt="16px">Website Under Construction</Text>
+                <Text className={styles.titleFont} mt="64px">Website Under Construction</Text>
                 <Center mt="16px">
                     <Text className={styles.subtitleFont}>While waiting for us, why not check out this trailer?</Text>
                 </Center>
