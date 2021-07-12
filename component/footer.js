04f51a8bd79ccc700b42f07fc35460a2c3b78f43
AtlanticNova <idiotcolony97@gmail.com>
2021-07-12 17:14:39 +0700

    Footer Responsive

--- component/footer.js
+++ component/footer.js
@@ -1,20 +1,17 @@
 import {
     Image,
-    Link,
     Flex,
     Text,
     Stack,
-    HStack
+    Center
 } from "@chakra-ui/react";
 import styles from "../styles/footer.module.scss"
 
 const Footer = ({isDesktop}) => {
     return(
-        <HStack h="192px" bgColor="#080818" w="100%" justifyContent="space-between" top={0}>
-            <Link href="/">
-                <Image src="./images/footerLogo.svg" alt="logo" ml="120px" cursor="pointer"/>
-            </Link>            
-            <Stack w="100%" alignItems="center" pr="300px" top={0}>
+        <Center w="100%" h="192px" bgColor="#080818" top={0}>
+            {isDesktop ? <Image src="./images/footerLogo.svg" className={styles.footerLogo} height={{base:"48px", sm: "92px", md: "128px"}}/> : <Image src="./images/footerLogo.svg" className={styles.footerLogo2}/>}
+            {isDesktop ? <Stack alignItems="center" >
                 <Flex>
                     <Image src="./images/instagram.svg" alt="instagram" cursor="pointer"/>
                     <Image src="./images/linkedIn.svg" alt="linkedIn" ml="16px" cursor="pointer"/>
@@ -23,8 +20,17 @@ const Footer = ({isDesktop}) => {
                 <Text fontSize="md" color="white" className={styles.footerFont}>
                         Copyright @2021 StudentsxCEOs 
                 </Text>
-            </Stack>
-        </HStack>
+            </Stack> : <Stack alignItems="center">
+                <Flex>
+                    <Image src="./images/instagram.svg" alt="instagram" cursor="pointer"/>
+                    <Image src="./images/linkedIn.svg" alt="linkedIn" ml="16px" cursor="pointer"/>
+                    <Image src="./images/twitter.svg" alt="twitter" ml="16px" cursor="pointer"/>
+                </Flex>
+                <Text fontSize="md" color="white" className={styles.footerFont} fontSize={{base:"14px", md: "16px"}}>
+                        Copyright @2021 StudentsxCEOs 
+                </Text>
+            </Stack>}
+        </Center>
     )
 }
 
