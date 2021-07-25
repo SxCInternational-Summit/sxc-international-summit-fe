cc9ca6924ab0b0375fa3a3c2a67828b1d1f95d8d
Daniel Andrew <danielandrewtt@gmail.com>
2021-07-25 10:35:07 +0700

    Added page title and handleGuidebook for download guidebook

--- component/hero.js
+++ component/hero.js
@@ -2,96 +2,108 @@ import { ChevronLeftIcon } from "@chakra-ui/icons"
 import { Button, Center, Text, Flex, useMediaQuery } from "@chakra-ui/react"
 import { useRouter } from "next/router"
 
-const Hero = ({title, subtitle, image, buttons}) => {
+import Head from "next/head"
+
+const Hero = ({title, subtitle, image, buttons, guidebookURL}) => {
     const [isMobile] = useMediaQuery("(max-width: 48em)")
     const router = useRouter()
+
+    const handleGuidebook = () => {
+        window.open(guidebookURL, '_blank');
+    }
+
     return(
-        <Center
-            backgroundImage={`url(${image})`}
-            mt="64px"
-            h={{base: "242px", md:"488px"}}
-            bgRepeat="no-repeat" 
-            bgSize="cover"
-            flexDirection="column">
+        <>
+            <Head>
+                <title>Competition | SxCInternational Summit 2021</title>
+            </Head>
+            <Center
+                backgroundImage={`url(${image})`}
+                mt="64px"
+                h={{base: "242px", md:"488px"}}
+                bgRepeat="no-repeat" 
+                bgSize="cover"
+                flexDirection="column">
 
-            {isMobile ? <></> :
-                <Button
-                    pos="absolute"
-                    top="20px"
-                    left="120px"
+                {isMobile ? <></> :
+                    <Button
+                        pos="absolute"
+                        top="20px"
+                        left="120px"
+                        color="white"
+                        bgColor="transparent"
+                        fontSize="1rem"
+                        lineHeight="150%"
+                        fontFamily="primaryFont"
+                        fontWeight="900"
+                        p="10px 12px"
+                        onClick={() => router.push("/event")}
+                        >
+                        <Flex lineHeight="100%">
+                            <ChevronLeftIcon />
+                            <Text ml="6px">Back to Events Page</Text>
+                        </Flex>
+                    </Button>
+                }
+
+                <Text 
+                    className="gradientFont"
+                    fontSize={{base: "24px", md:"64px"}}
+                    lineHeight={{base:"28px", md:"68px"}}
+                    w="40%"
+                    textAlign="center"
+                    >
+                    {title}
+                </Text>
+
+                <Text
+                    w="40%"
+                    className="secondaryFont"
                     color="white"
-                    bgColor="transparent"
-                    fontSize="1rem"
+                    fontSize={{base: "0.75rem", md:"1rem"}}
                     lineHeight="150%"
-                    fontFamily="primaryFont"
-                    fontWeight="900"
-                    p="10px 12px"
-                    onClick={() => router.push("/event")}
+                    fontWeight="400"
+                    textAlign="center"
+                    mt={{base: "0.75rem", md:"2rem"}}
                     >
-                    <Flex lineHeight="100%">
-                        <ChevronLeftIcon />
-                        <Text ml="6px">Back to Events Page</Text>
-                    </Flex>
-                </Button>
-            }
+                    {subtitle}
+                </Text>
 
-            <Text 
-                className="gradientFont"
-                fontSize={{base: "24px", md:"64px"}}
-                lineHeight={{base:"28px", md:"68px"}}
-                w="40%"
-                textAlign="center"
-                >
-                {title}
-            </Text>
-
-            <Text
-                w="40%"
-                className="secondaryFont"
-                color="white"
-                fontSize={{base: "0.75rem", md:"1rem"}}
-                lineHeight="150%"
-                fontWeight="400"
-                textAlign="center"
-                mt={{base: "0.75rem", md:"2rem"}}
-                >
-                {subtitle}
-            </Text>
-
-            {buttons ? 
-            <Center>
-                <Button
-                    onClick={() => router.push("competition/guidebook")}
-                    mt="16px"
-                    color="black"
-                    bgColor="#F8C800"
-                    p="10px 24px"
-                    className="tertiaryFont"
-                    fontWeight="bold"
-                    fontSize="16px"
-                    lineHeight="20px"
-                    >
-                    Event Guidebook
-                </Button>
-                <Button
-                    onClick={() => router.push("competition/register-team")}
-                    mt="16px"
-                    ml="20px"
-                    color="black"
-                    bgColor="#F8C800"
-                    p="10px 24px"
-                    className="tertiaryFont"
-                    fontWeight="bold"
-                    fontSize="16px"
-                    lineHeight="20px"
-                    >
-                    Register Now
-                </Button>
+                {buttons ? 
+                <Center>
+                    <Button
+                        onClick={handleGuidebook}
+                        mt="16px"
+                        color="black"
+                        bgColor="#F8C800"
+                        p="10px 24px"
+                        className="tertiaryFont"
+                        fontWeight="bold"
+                        fontSize="16px"
+                        lineHeight="20px"
+                        >
+                        Event Guidebook
+                    </Button>
+                    <Button
+                        onClick={() => router.push("competition/register-team")}
+                        mt="16px"
+                        ml="20px"
+                        color="black"
+                        bgColor="#F8C800"
+                        p="10px 24px"
+                        className="tertiaryFont"
+                        fontWeight="bold"
+                        fontSize="16px"
+                        lineHeight="20px"
+                        >
+                        Register Now
+                    </Button>
+                </Center>
+                :
+                <></>
+                }
             </Center>
-            :
-            <></>
-            }
-        </Center>
+        </>
     )
 }
 
