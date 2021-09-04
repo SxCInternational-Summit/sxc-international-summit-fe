2f7e51458e3d40aa65eba0de7394564cecf55aaf
Daniel Andrew <danielandrewtt@gmail.com>
2021-09-04 13:28:15 +0700

    No longer fetch events from backend

--- pages/event/index.js
+++ pages/event/index.js
@@ -8,7 +8,7 @@ import Navbar from "../../component/navbar";
 import { NextSeo } from "next-seo";
 
 const Events = ({events}) => {
-	const faq =[
+	const faq = [
 		{
 			question: "How many events there are in SxcInternational Summit 2021?",
 			answer: "There are 3 events. Pre-event (webinar), Competition, and Main Event"
@@ -22,31 +22,62 @@ const Events = ({events}) => {
 			answer: "Exciting new experience, insights, and even new connections."
 		}
 	]
+
+	const event = [
+		{
+			title: "Maintain product existence in the middle of global pandemic",
+            src: "/images/event1.svg",
+            subtitle: "To engage and reach more of aspiring Gen-Zs that are interested in developing their own brand.  The webinar will cover how to maintain product existence 101, especially through the challenges in this uncertain era of global pandemic",
+            type: "Pre-Event",
+            date: "28 August 2021",
+            fee: "Free",
+            linkTo: "https://sxcintersummit.com/pre-event"
+		},
+		{
+			title: "Product Branding Competition",
+            src: "/images/event1.svg",
+            subtitle: "SxC International Summit 2021 Product Branding Competition. Click Join Event for more info",
+            type: "Competition",
+            date: "26 July 2021 - 10 October 2021",
+            fee: "Closed",
+            linkTo: "https://sxcintersummit.com/competition"
+		},
+		// {
+		// 	title: "SxC International Summit Main Events",
+        //     src: "/images/event1.svg",
+        //     subtitle: "List of this year summit`s main event. Click Join Event for more info",
+        //     type: "Main Event",
+        //     date: "9 October 2021 - 16 October 2021",
+        //     fee: "Learn More",
+        //     linkTo: "https://sxcintersummit.com/main-event"
+		// }
+	]
+
 	return (
 		<>
 		<NextSeo 
 			title="Events List | SxCInternational Summit 2021" />
 		<Box>
 			<Navbar />
-			<EventList key={events.id} events={events}/>
+			<EventList events={event}/>
 			<Faq faqList={faq} title={true} />
 		</Box>
 		</>
 	)
 }
 
-export async function getServerSideProps() {
-	const { API_URL } = process.env
+// export async function getServerSideProps() {
+// 	const { API_URL } = process.env
 
-	const res = await fetch(`${API_URL}/events`)
-	const data = await res.json()
+// 	const res = await fetch(`${API_URL}/events`)
+// 	const data = await res.json()
 
-	return {
-		props: {
-			events: data
-		},
-	}
-}
+// 	return {
+// 		props: {
+// 			events: data
+// 		},
+// 	}
+// }
 
 export default Events
 	
